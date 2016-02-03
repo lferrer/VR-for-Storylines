22a1d59e558a8bf3ba7b7099b5df60c22d7ed4db
Leonardo Ferrer <ferrer@ucdavis.edu>
2016-02-03 15:48:52 -0800

    Project creation + Git testing.

---
+++ Assets/Scripts/DataImporter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.IO;
+
+public class DataImporter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var lines = File.ReadAllLines("test.txt").Select(a => a.Split(';'));
+        var csv = from line in lines
+                  select (from piece in line
+                          select piece);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
